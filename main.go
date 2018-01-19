f6252ebb07cee41d961b83ca08b15a18fae05cc1
Kinka <kinkabrain@gmail.com>
2018-01-19 12:54:01 +0000

    fix: remove binary file

--- main.go
+++ main.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+    "strings"
     ctrl "controllers"
 )
 
+func dispatch(w http.ResponseWriter, r *http.Request) {
+    p := r.URL.String()
+    
+    if strings.HasPrefix(p, "/share?") {
+        ctrl.Share(w, r)
+    } else if strings.HasPrefix(p, "/copy?") {
+        ctrl.Copy(w, r)
+    } else {
+        ctrl.Index(w, r)        
+    }
+}
+
 func main() {
-    http.HandleFunc("/", ctrl.Index)
-	http.HandleFunc("/share", ctrl.Share)
-	http.HandleFunc("/copy", ctrl.Copy)
+    http.HandleFunc("/", dispatch)
+	//http.HandleFunc("/share", ctrl.Share)
+	//http.HandleFunc("/copy", ctrl.Copy)
 
     fmt.Println("start listening...")
 	err := http.ListenAndServe(":9090", nil)
